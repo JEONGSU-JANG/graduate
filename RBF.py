c5554cf78be7edc3f5ecac063e7fa09b8d00e1a8
JEONGSU-JANG <jjs3288@gmail.com>
2018-04-04 09:58:52 +0900

    Add files via upload

--- RBF.py
+++ RBF.py
@@ -1,14 +1,12 @@
 from scipy import *
-from scipy.linalg import pinv
-import sys
-import openpyxl
-
+from scipy.linalg import norm, pinv
+from matplotlib import pyplot as plt
+import numpy as np
 
 class RBF:
     def __init__(self, indim, outdim):
         self.indim=indim
         self.outdim=outdim
-        self.numTest=len(indim)
         self.numStatement=len(indim[0])
         self.numCenters=0
         self.centers=[]
@@ -48,11 +46,7 @@ class RBF:
             ci=ci+c[i]**2
             ui=ui+u[i]**2
             cui=cui+c[i]*u[i]
-        temp=sqrt(ci*ui)
-        if(temp!=0):
-            cos=double(cui/temp)
-        else:
-            cos=0
+        cos=cui/(sqrt(ci*ui))
         return sqrt(1-cos)
 
 
@@ -62,41 +56,44 @@ class RBF:
 
     def train(self):
         self._center()
-        print "center :",self.numCenters
         self._sigma()
-        print "sigma :",self.sigma
         G=[]
-        for i in range(self.numTest):
+        for i in range(len(self.indim)):
             A=[]
-            for j in range(self.numCenters):
+            for j in range(len(self.centers)):
                 A.append(self._basicfunc(self.indim[i], self.centers[j]))
             G.append(A)
         self.W=dot(pinv(G), self.outdim)
-        print "weight :",self.W
 
 
-    def test(self):
-        j=0
-        k=1
-        Y=[]
-       
-        while(True):
-            if(j==self.numStatement):
-                break        
-            
-            temp=[0 for value in range(self.numStatement)]
-            temp[j]=j
-                
+    def test(self, X):
+        vt=eye(X)
+        G=[]
+        for i in range(len(vt)):
             A=[]
-            for i in range(self.numCenters):
-                A.append(self._basicfunc(temp, self.centers[i]))
-
-            Y.append([dot(A, self.W),k])
-            print "Suspicious ",k," :",dot(A,self.W)
-            j=j+1
-            k=k+1
-
+            for j in range(len(self.centers)):
+                A.append(self._basicfunc(vt[i], self.centers[j]))
+            G.append(A)
+        Y=dot(G, self.W)
         return Y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -106,7 +103,7 @@ def swap(x, i, j):
     x[i],x[j]=x[j],x[i]
 
 
-def quicksort(x, left, right): #left is left index, right is right index ex)x[1,2,3,4,5]-> left=0, right=4
+def quicksort(x, index, left, right): #left is left index, right is right index ex)x[1,2,3,4,5]-> left=0, right=4
     pivot=left
     j=pivot
 
@@ -114,105 +111,52 @@ def quicksort(x, left, right): #left is left index, right is right index ex)x[1,
         for i in range(left+1, right+1):
             # starting with i+1, we will check all value in array 'x'
             # for ascending, if x[i]<x[pivot], we will swap x[i] and x[j], because j is smaller than i always.
-            if(x[i]>x[pivot]):
+            if(x[i]<x[pivot]):
                 j=j+1
                 swap(x, i, j)
+                swap(index, i, j)
 
         swap(x, left, j) # swap x[pivot] and x[j] for complete
+        swap(index, left, j)
         pivot=j
 
-        quicksort(x, left, pivot-1) # do again the quicksort with left~pivot-1
-        quicksort(x, pivot+1, right)  #do again the quicksort with pivot+1~right
+        quicksort(x, index, left, pivot-1) # do again the quicksort with left~pivot-1
+        quicksort(x, index, pivot+1, right)  #do again the quicksort with pivot+1~right
 
 
-
-
-
-
-
-
-
-
-
-
-
-def weightFileOpen(outtrain, source):
-    f=open(source, "r")
-
-    while True:
-        line=f.readline()
-        if not line: break
-        temp=line.split("\n")
-        outtrain.append([float(temp[0])])
-    f.close()
-
-
-def traceFileOpen(intrain, outtrain, fileN, version):
-    excel_document=openpyxl.load_workbook(filename=fileN, read_only=True)
-    ver='v'+str(version)
-    sheet=excel_document.get_sheet_by_name(ver)
-
-    Matrix=[]
-    tempM=[]
-    for row in sheet.iter_rows():
-        for cell in row:
-            cell_text=cell.value
-            tempM.append(cell_text)
-        Matrix.append(tempM)
-        tempM=[]
-
-    rows=len(Matrix)
-    columns=len(Matrix[0])
-
-    tempIn=[]
-
-    for c in range(2, columns):
-        for r in range(2, rows-1):
-            if Matrix[r][c]==0L or Matrix[r][c]=="0":
-                tempIn.append(0)
-            elif Matrix[r][c]==1L or Matrix[r][c]=="1":
-                tempIn.append(1)
-
-        if tempIn!=[]:
-            intrain.append(tempIn)
-        tempIn=[]
-
-    for c in range(2, columns):
-        if Matrix[rows-1][c]=="PASS":
-            outtrain.append([0])
-        elif Matrix[rows-1][c]=="FAIL":
-            outtrain.append([1])
 
 if __name__=="__main__":
-    sys.setrecursionlimit(1000000000)
+   
+    intrain = [[1,1,1,1,0,1,0,0,1,1],
+               [1,0,0,1,1,0,1,0,0,1],
+               [1,1,1,0,0,1,0,0,1,1],
+               [1,0,1,0,0,1,1,0,1,1],
+               [1,1,1,0,1,0,0,0,1,0],
+               [1,1,1,1,0,0,0,1,1,1],
+               [1,0,1,1,1,1,1,1,0,1]]
 
-    version=1
-    while(version<4):
-        intrain=[]
-        outtrain=[]
-        fileN="C:/Users/Jeongsu Jang/Desktop/2018-1/paper/RBFtest/printtokens/printtokens.xlsx"
-        print fileN
-        traceFileOpen(intrain, outtrain, fileN, version)
-
-        outtrain=[]
-        source="C:/Users/Jeongsu Jang/Desktop/2018-1/paper/pageRankTest/printtokens/v"+str(version)+".txt"
-        weightFileOpen(outtrain,source)
-
-        LOC=len(intrain[0])
-        rbf = RBF(intrain, outtrain)
-        print "training start"
-        rbf.train()
-        z=rbf.test()
-
-        save='C:/Users/Jeongsu Jang/Desktop/2018-1/paper/RBFtest/printtokens/Weight_v'+str(version)+'.txt'
-        f=open(save, 'w+')
-
-        quicksort(z, 0, LOC-1)
-        print "Ranking---------------"
-        for i in range(LOC):
-            print z[i][1]
-            ranking=str(z[i][1]) + "\n"
-            f.write("suspiciousness:"+str(z[i][0])+":number:"+ranking)
-
-        f.close()
-        version=version+1
+    outtrain = [[0],
+                [0],
+                [0],
+                [0],
+                [0],
+                [1],
+                [1]]
+    
+    rbf = RBF(intrain, outtrain)
+    rbf.train()
+    z=rbf.test(len(intrain[0]))
+    print "Suspiciousness :", z
+    index=[]
+    for i in range(len(z)):
+        index.append(i+1)
+    quicksort(z, index, 0, len(z)-1)
+    index.reverse()
+    print "Ranking :", index
+    print " "
+    print "sigma :", rbf.sigma
+    print " "
+    print "Number of Centers :", rbf.numCenters
+    print "centers :", rbf.centers
+    print " "
+    print "weight :", rbf.W
